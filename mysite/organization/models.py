40debc770789709096702b52f4c062aaebb8fbb6
edvin <edvinkeskin@gmail.com>
2023-09-18 16:14:29 -0700

    removed redundancy by adding mixer to model

--- mysite/organization/models.py
+++ mysite/organization/models.py
@@ -2,38 +2,32 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Employee(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class AddressMixin(models.Model):
     address = models.CharField(max_length=200, default="123 Road")
     city = models.CharField(max_length=200, default="Vancouver")
     province = models.CharField(max_length=200, default="BC")
     postal_code = models.CharField(max_length=200, default="V6T1Z1")
     country = models.CharField(max_length=200, default="Canada")
     phone_no = models.CharField(max_length=200, default="6045254590")
+
+    class Meta:
+        abstract = True
+
+
+class Employee(AddressMixin):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
 
 
-class Office(models.Model):
+class Office(AddressMixin):
     name = models.CharField(max_length=200, default="office a")
-    address = models.CharField(max_length=200, default="123 Road")
-    city = models.CharField(max_length=200, default="Vancouver")
-    province = models.CharField(max_length=200, default="BC")
-    postal_code = models.CharField(max_length=200, default="V6T1Z1")
-    country = models.CharField(max_length=200, default="Canada")
-    phone_no = models.CharField(max_length=200, default="6045254590")
     employees = models.ManyToManyField(Employee)
 
 
-class Organization(models.Model):
+class Organization(AddressMixin):
     name = models.CharField(max_length=200)
-    address = models.CharField(max_length=200, default="123 Road")
-    city = models.CharField(max_length=200, default="Vancouver")
-    province = models.CharField(max_length=200, default="BC")
-    postal_code = models.CharField(max_length=200, default="V6T1Z1")
-    country = models.CharField(max_length=200, default="Canada")
-    phone_no = models.CharField(max_length=200, default="6045254590")
     offices = models.ManyToManyField(Office)
 
     def __str__(self):
